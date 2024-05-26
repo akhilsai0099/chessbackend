5b3e33dfbe5c1856bd908685491f45319b103e74
akhilsai0099 <akhilsai0099@gmail.com>
2024-05-26 15:59:14 +0530

    changed gitignore

--- dist/Messages.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UPDATE = exports.GAME_OVER = exports.RESET_BOARD = exports.MOVE = exports.INIT_GAME = void 0;
-exports.INIT_GAME = "init";
-exports.MOVE = "move";
-exports.RESET_BOARD = "RESET_BOARD";
-exports.GAME_OVER = "game over";
-exports.UPDATE = "update";
